0be53e7fbb536bc0226e37ebea4e2f22d27c897b
Rishi51199 <rishi51199@gmail.com>
2015-10-12 20:24:36 -0700

    Create onlineMedianLambda.java

---
+++ onlineMedianLambda.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nintegers = scanner.nextInt();
+
+        MedianCalculator medianCalculator = new MedianCalculator();
+
+        for (int i = 0; i < nintegers; i++) { //O(n)
+            medianCalculator.addNumber(scanner.nextInt());
+            System.out.println(medianCalculator.calculateMedian());
+        }
+    }
+
+    private static class MedianCalculator {
+        Queue<Integer> maxheap = new PriorityQueue<>((o1, o2) -> o2.compareTo(o1)); //ascending order
+        Queue<Integer> minheap = new PriorityQueue<>((o1, o2) -> o1.compareTo(o2)); //descending order
+
+        public void addNumber(int number) {
+
+            if(maxheap.isEmpty() || number < maxheap.peek()) {
+                maxheap.add(number);
+            } else {
+                minheap.add(number);
+            }
+
+            //balance both sides
+            if(maxheap.size() > minheap.size() + 1) {
+                minheap.add(maxheap.poll());
+            }
+
+            if(minheap.size() > maxheap.size() + 1) {
+                maxheap.add(minheap.poll());
+            }
+        }
+
+        public double calculateMedian()
+        {
+            if(maxheap.size() == minheap.size()) {
+                return (maxheap.peek() + minheap.peek()) / 2.0;
+            }
+            else {
+                return (double) maxheap.size() > minheap.size() ? maxheap.peek() : minheap.peek();
+            }
+        }
+    }
+}
